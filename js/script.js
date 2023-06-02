237976c73bb4e622c55d730152523e4d8f8596e7
f3lip3pavs <felipepavanm@gmail.com>
2023-06-01 22:36:17 -0300

    typing

--- js/script.js
+++ js/script.js
@@ -48,9 +48,9 @@ ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
 ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
 
 const typed = new Typed('.multiple-text', {
-    strings: ['Escola da Nuvem'],
-    typeSpeed: 100,
-    backSpeed: 100,
-    backDelay: 1000,
+    strings: ['<span>Escola da Nuvem</span>'],
+    typeSpeed: 40,
+    backSpeed: 0,
+    fadeOut: true,
     loop: true
 });
